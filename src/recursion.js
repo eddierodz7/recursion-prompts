6c38d8385922568173445820441a1d931034dc0a
eddierodz7 <eddierodz7@gmail.com>
2018-07-04 18:00:28 -0500

    finshed 4 working on 5

--- src/recursion.js
+++ src/recursion.js
@@ -66,6 +66,13 @@ if(n < 1){
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+  if(n === 0){
+    return 0;
+    }
+    if(n < 0){
+      return sumBelow(n * -1);
+  }
+  return n + sumBelow(n - 1)
 };
 
 // 6. Get the integers within a range (x, y).
