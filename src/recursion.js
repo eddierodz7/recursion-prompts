ac0c2a723837dc36e154a7e709caa178ee651863
eddierodz7 <eddierodz7@gmail.com>
2018-07-05 18:03:29 -0500

    saving work on 15

--- src/recursion.js
+++ src/recursion.js
@@ -239,6 +239,10 @@ return gcd(y, x % y);
 // compareStr('house', 'houses') // false
 // compareStr('tomato', 'tomato') // true
 var compareStr = function(str1, str2) {
+  if(str1.length !== str2.length){
+    return false;
+  }
+
 };
 
 // 16. Write a function that accepts a string and creates an array where each letter
