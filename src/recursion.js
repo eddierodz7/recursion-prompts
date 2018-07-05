da9d28f303332b74163ea419533d232b7a95c01c
eddierodz7 <eddierodz7@gmail.com>
2018-07-05 16:45:27 -0500

    finished 13&14

--- src/recursion.js
+++ src/recursion.js
@@ -225,7 +225,13 @@ if(x,y < 0){
 // http://www.cse.wustl.edu/~kjg/cse131/Notes/Recursion/recursion.html
 // https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/the-euclidean-algorithm
 var gcd = function(x, y) {
-
+  if(y < 0 || x < 0 ){
+    return null;
+  }
+  if(y % x === 0){
+    return x;
+  }
+return gcd(y, x % y);
 };
 
 // 15. Write a function that compares each character of two strings and returns true if
